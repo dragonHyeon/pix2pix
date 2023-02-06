139a268f93bb697e35571a3cfbcca768e240c48d
dragonhyeon <kyh.mv@movements.kr>
2023-02-06 14:36:58 +0900

    5: PatchGAN implemented

--- DeepLearning/layer.py
+++ DeepLearning/layer.py
@@ -77,3 +77,37 @@ class UNetUp(nn.Module):
         x = torch.cat(tensors=(x, skip), dim=1)
 
         return x
+
+
+class DisBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, normalize=True):
+        """
+        * DisBlock 모듈 구조 정의
+        :param in_channels: in_channels 수
+        :param out_channels: out_channels 수
+        :param normalize: InstanceNorm2d 여부
+        """
+
+        super(DisBlock, self).__init__()
+
+        # Conv2d
+        layers = [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1, bias=True)]
+        # InstanceNorm2d
+        if normalize:
+            layers.append(nn.InstanceNorm2d(num_features=out_channels))
+        # LeakyReLU
+        layers.append(nn.LeakyReLU(negative_slope=0.2))
+
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        """
+        * 순전파
+        :param x: 배치 개수 만큼의 입력. (N, in_channels, H, W)
+        :return: 배치 개수 만큼의 출력. (N, out_channels, H/2, W/2)
+        """
+
+        # (N, in_channels, H, W) -> (N, out_channels, H/2, W/2)
+        x = self.block(x)
+
+        return x
--- DeepLearning/model.py
+++ DeepLearning/model.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn
 
-from DeepLearning.layer import UNetUp, UNetDown
+from DeepLearning.layer import UNetUp, UNetDown, DisBlock
 
 
 class GeneratorUNet(nn.Module):
@@ -13,7 +14,7 @@ class GeneratorUNet(nn.Module):
 
         super(GeneratorUNet, self).__init__()
 
-        # (N, in_channels (3), H, W) -> (N, 64, H/2, W/2)
+        # (N, in_channels (3), H (256), W (256)) -> (N, 64, H/2, W/2)
         self.down1 = UNetDown(in_channels=in_channels, out_channels=64, normalize=False)
         # (N, 64, H/2, W/2) -> (N, 128, H/4, W/4)
         self.down2 = UNetDown(in_channels=64, out_channels=128)
@@ -44,7 +45,7 @@ class GeneratorUNet(nn.Module):
         self.up6 = UNetUp(in_channels=512, out_channels=128)
         # (N, 256, H/4, W/4) -> (N, 64, H/2, W/2)
         self.up7 = UNetUp(in_channels=256, out_channels=64)
-        # (N, 128, H/2, W/2) -> (N, out_channels (3), H, W)
+        # (N, 128, H/2, W/2) -> (N, out_channels (3), H (256), W (256))
         self.up8 = nn.Sequential(
             nn.ConvTranspose2d(in_channels=128, out_channels=out_channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
@@ -53,11 +54,11 @@ class GeneratorUNet(nn.Module):
     def forward(self, x):
         """
         * 순전파
-        :param x: 배치 개수 만큼의 입력. (N, in_channels (3), H, W)
-        :return: 배치 개수 만큼의 변환된 이미지. (N, out_channels (3), H, W)
+        :param x: 배치 개수 만큼의 입력. (N, in_channels (3), H (256), W (256))
+        :return: 배치 개수 만큼의 변환된 이미지. (N, out_channels (3), H (256), W (256))
         """
 
-        # (N, in_channels (3), H, W) -> (N, 64, H/2, W/2)
+        # (N, in_channels (3), H (256), W (256)) -> (N, 64, H/2, W/2)
         d1 = self.down1(x)
         # (N, 64, H/2, W/2) -> (N, 128, H/4, W/4)
         d2 = self.down2(d1)
@@ -88,52 +89,45 @@ class GeneratorUNet(nn.Module):
         u6 = self.up6(u5, d2)
         # (N, 256, H/4, W/4) -> (N, 128, H/2, W/2)
         u7 = self.up7(u6, d1)
-        # (N, 128, H/2, W/2) -> (N, out_channels (3), H, W)
+        # (N, 128, H/2, W/2) -> (N, out_channels (3), H (256), W (256))
         u8 = self.up8(u7)
 
         return u8
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=3):
         """
         * 모델 구조 정의
+        :param in_channels: in_channels 수
         """
 
         super(Discriminator, self).__init__()
 
-        # 판별자
+        # 16 x 16 PatchGAN
         self.main = nn.Sequential(
-            # (N, 3, 64, 64) -> (N, 128, 32, 32)
-            nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            # (N, 128, 32, 32) -> (N, 256, 16, 16)
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=256),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            # (N, 256, 16, 16) -> (N, 512, 8, 8)
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=512),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            # (N, 512, 8, 8) -> (N, 1024, 4, 4)
-            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=1024),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            # (N, 1024, 4, 4) -> (N, 1, 1, 1)
-            nn.Conv2d(in_channels=1024, out_channels=1, kernel_size=4, stride=1, padding=0, bias=False),
+            # (N, in_channels*2 (6), H (256), W (256)) -> (N, 64, H/2, W/2)
+            DisBlock(in_channels=in_channels * 2, out_channels=64, normalize=False),
+            # (N, 64, H/2, W/2) -> (N, 128, H/4, W/4)
+            DisBlock(in_channels=64, out_channels=128),
+            # (N, 128, H/4, W/4) -> (N, 256, H/8, W/8)
+            DisBlock(in_channels=128, out_channels=256),
+            # (N, 256, H/8, W/8) -> (N, 512, H/16, W/16)
+            DisBlock(in_channels=256, out_channels=512),
+            # (N, 512, H/16, W/16) -> (N, 1, H/16 (16), W/16 (16))
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
-    def forward(self, x):
+    def forward(self, a, b):
         """
         * 순전파
-        :param x: 배치 개수 만큼의 입력. (N, 3, 64, 64)
-        :return: 배치 개수 만큼의 참 거짓 판별 결과. (N)
+        :param a: 배치 개수 만큼의 입력. (N, in_channels (3), H (256), W (256))
+        :param b: 배치 개수 만큼의 입력. (N, in_channels (3), H (256), W (256))
+        :return: 배치 개수 만큼의 16 x 16 patch 의 참 거짓 판별 결과. (N, 1, H/16 (16), W/16 (16))
         """
 
-        # (N, 3, 64, 64) -> (N, 1, 1, 1)
-        x = self.main(x)
-        # (N, 1, 1, 1) -> (N)
-        x = x.view(-1)
+        # (N, in_channels+in_channels (6), H (256), W (256)) -> (N, 1, H/16 (16), W/16 (16))
+        x = self.main(torch.cat(tensors=(a, b), dim=1))
 
         return x
